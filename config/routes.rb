87980b9c6814a1af9e9e60c71ce65167600de45a
ANDRESTA02 <andresta02@hotmail.com>
2014-10-01 20:04:40 -0500

    Creacion de logueo, logueo alineado a la derecha

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+
+  get 'user_sessions/create'
+
+  get 'user_sessions/destroy'
+
+  resources :users
+
   resources :estados
 
   resources :ambientes
@@ -16,6 +24,13 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'registros#index'
+
+
+resources :user_sessions
+resources :users
+
+get 'login' => 'user_sessions#new', :as => :login
+get 'logout' => 'user_sessions#destroy', :as => :logout
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
