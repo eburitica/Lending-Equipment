1d4f96620f64723ae5e5d2e9e8e70a31da56d8c2
Esteban <baneste.17@gmail.com>
2014-11-21 21:47:27 -0500

    Poblacion en la base de datos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+tipo_elementos = [{nombre:"Portatiles",descripcion:"",icono:"fa-laptop"},
+{nombre:"Cargadores de portatil",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Mouses",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Cables HDMI",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Cables VGA",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Diademas",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Pantalla-TV",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Control de pantalla",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Control de aire",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Microfonos",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Equipo de sonido",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Bafles de portatil",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Video-Beam",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Pantalla Video-Beam",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Extenciones",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Llaves",descripcion:"",icono:"fa-shopping-cart"},
+{nombre:"Astas",descripcion:"",icono:"fa-shopping-cart"}]
+
+
+tipo_elementos.each do |tipo_elemento|
+	TipoElemento.where(nombre: tipo_elemento[:nombre], icono: tipo_elemento[:icono]).first || TipoElemento.create(tipo_elemento)
+end
